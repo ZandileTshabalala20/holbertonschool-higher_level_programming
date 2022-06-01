dc6f3db4c46da65309dc9121d6c36c2c3e5e3f11
Zandile Tshabalala <zTshabalala@SINGULAR.CO.ZA>
2022-06-01 08:50:48 +0200

    commit files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""read_file
-"""
+
+'''Task 00 - 0. Read file'''
 
 
 def read_file(filename=""):
-    """Takes in str filename to read it
-    """
-
-    with open(filename, encoding="utf-8") as readFile:
-        print(readFile.read(), end='')
+    '''function that reads a text file (UTF8)
+    and prints it to stdout:'''
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end="")
